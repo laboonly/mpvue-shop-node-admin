0d0fc1e705524e46d98f3159235cbe59b4bc6a44
刘毅 <326534215@qq.com>
2020-10-22 02:59:10 +0800

    new: 编写出第一个api

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,7 +1,15 @@
 const { mysql } = require('../../mysql')
 
-module.exports = async (ctx) => {
+module.exports = async (ctx) => { 
+    // 轮播图数据
+    const banner = await mysql('nideshop_ad').where({
+        ad_position_id: 1
+    }).select();
+    //类型
+    const channel = await mysql('nideshop_channel').select();
+
     ctx.body = {
-        "banner": 'banner'
+        "banner": banner,
+        "channel": channel
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@
 */
 
 const router = require('koa-router')({
-    prefix: '/heyushuo'
+    prefix: '/ly'
 })
 // const router = new Router()
+
 const controllers = require('../controllers')
+
+//================================================================自己的接口
 
 // 首页数据
 router.get('/index/index', controllers.home.index)
