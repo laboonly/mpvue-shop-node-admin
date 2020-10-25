4ac017895bb38da81c5281926bfa856712a2fa96
刘毅 <326534215@qq.com>
2020-10-25 16:31:41 +0800

    new: 搜索页面的api

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const Koa = require('koa')
-
+const debug = require('debug')('koa-weapp-demo')
 const config = require('./config')
-
+const response = require('./middlewares/response')
+const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 
-
+// 使用响应处理中间件
+app.use(response)
+// 解析请求体
+app.use(bodyParser())
 //路由分发
 const router = require('./routes')
 app.use(router.routes())
-
+// 启动程序，监听端口
 app.listen(config.port, () => {console.log(`listening on port ${config.port}`)})
--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,18 @@ const controllers = require('../controllers')
 router.get('/index/index', controllers.home.index)
 
 
+
+
+/**
+ * 搜索相关接口
+ */
+//1.关键词和搜索历史接口
+router.get('/search/indexaction', controllers.search.index.indexAction)
+//2.搜索提示接口
+router.get('/search/helperaction', controllers.search.index.helperAction)
+//3.搜索的关键词添加到数据库
+router.post('/search/addhistoryaction', controllers.search.index.addHistoryAction)
+//4.清空搜索历史
+router.post('/search/clearhistoryAction', controllers.search.index.clearhistoryAction)
+
 module.exports = router
